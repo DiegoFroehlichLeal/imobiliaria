d1627ba0378f61d59577b022ed467e3cadf7cf04
Diego F. Leal <diegofrleal@gmail.com>
2024-12-04 19:52:44 -0300

    Refatora estilos do cabeçalho e adiciona responsividade para dispositivos móveis

--- templates/header.php
+++ templates/header.php
@@ -1,11 +1,9 @@
 <header>
     <div class="header-container">
         <img src="resources/LOGO500X500.png" alt="Logo Imobiliária" class="logo"> <!-- Adicione o caminho para o logo -->
-        <img src="resources/whatsapp.png" alt="Logo Imobiliária" class="logo">
         <nav>
             <a href="index.php">Início</a>
             <a href="sobre.php">Sobre</a>
-            <a href="#">Contatos</a>
         </nav>
     </div>
 </header>
