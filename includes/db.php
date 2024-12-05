9e859d06cf193db877069ea025eecba98893b81e
Diego Froehlich Leal <diegofrleal@gmail.com>
2024-12-05 20:44:56 -0300

    Adiciona suporte ao uso de variáveis de ambiente com o pacote phpdotenv e refatora a conexão com o banco de dados

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,20 @@
 <?php
-$host = "srv1660.hstgr.io"; // Ou o nome do servidor MySQL, se diferente
-$user = "u773098555_user_imob";
-$pass = "@Vigia77895677";
-$db = "u773098555_imobiliaria_db";
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// Carrega as variáveis de ambiente do arquivo .env
+$dotenv = Dotenv::createImmutable(__DIR__ );
+$dotenv->load();
+
+// Acessa as variáveis de ambiente corretamente
+$host = $_ENV['DB_host'];
+$user = $_ENV['DB_user'];
+$pass = $_ENV['DB_pass'];
+$db   = $_ENV['DB_db'];
+
+// Inicia a conexão com o banco de dados
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
