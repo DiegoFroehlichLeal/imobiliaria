d4ddf0f21b414a6ae98531294a60e011de82ff5e
Diego Froehlich Leal <diegofrleal@gmail.com>
2024-12-07 02:33:13 -0300

    refatora cabeçalho e estilos, melhora a estrutura do grid e atualiza imagens de fundo

--- templates/header.php
+++ templates/header.php
@@ -1,19 +1,25 @@
 <header>
-    <div class="header-container">
-        <div class="grid-item"><img src="resources/LOGO500X500.png" alt="Logo Imobiliária" id="logo"></div>
-        <div class="grid-item"><img src="/resources/whatsapp.png" alt=""></div>
-        <div class="grid-item">
-            <p>(47) 99142-4641</p>
-        </div>
-        <div class="grid-item"><img src="/resources/telefoneFixo.png" alt=""></div>
-        <div class="grid-item">
-            <p>(47) 3084-0707</p>
-        </div>
-        <div class="grid-item">
-            <nav>
-                <a href="index.php">Início</a>
-                <a href="sobre.php">Sobre</a>
-            </nav>
-        </div>
+  <div class="header-container">
+    <!-- Item 1: Logo -->
+    <div class="grid-item logo">
+      <img src="resources/LOGO500X500.png" alt="Logo Imobiliária" id="logo">
     </div>
+    <!-- Item 2 e 3: Ícone e texto do primeiro telefone -->
+    <div class="grid-item contact">
+      <img src="resources/whatsapp.png" alt="WhatsApp">
+      <p>(47) 99142-4641</p>
+    </div>
+    <!-- Item 4 e 5: Ícone e texto do segundo telefone -->
+    <div class="grid-item contact">
+      <img src="resources/telefoneFixo.png" alt="Telefone Fixo">
+      <p>(47) 3084-0707</p>
+    </div>
+    <!-- Item 6: Menu -->
+    <div class="grid-item menu">
+      <nav>
+        <a href="index.php">Início</a>
+        <a href="sobre.php">Sobre</a>
+      </nav>
+    </div>
+  </div>
 </header>
