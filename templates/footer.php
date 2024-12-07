d8144328fbe089da6da51e274722e8856f64aae8
Diego Froehlich Leal <diegofrleal@gmail.com>
2024-12-07 03:28:08 -0300

    adiciona novo rodapé com informações de contato, logo e direitos autorais, e refatora estilos do footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,33 @@
-</main>
 <footer>
-    <p>&copy; <?= date('Y'); ?> Imobiliária. Todos os direitos reservados.</p>
+    <div class="footer-container">
+        <!-- Logo -->
+        <div class="footer-logo">
+            <img src="resources/LOGO500X500.png" alt="Logo Imobiliária">
+        </div>
+        <!-- Informações adicionais -->
+        <ul class="footer-info">
+            <li>
+                <img src="resources/mapGreen.png" alt="Endereço">
+                R. João Carlos Stein, 593 - Jaraguá Esquerdo, Jaraguá do Sul - SC, 89253-350
+            </li>
+            <li>
+                <img src="resources/whatsapp.png" alt="WhatsApp">
+                (47) 99142-4641
+            </li>
+            <li>
+                <img src="resources/telefoneFixo.png" alt="Telefone Fixo">
+                (47) 3084-0707
+            </li>
+            <li>
+                <img src="resources/emailGreen.png" alt="Email">
+                contato@isimoveis.com.br
+            </li>
+            <li>
+                <img src="resources/clockGreen.png" alt="Horário">
+                08:30 às 12:00 - 13:30 às 18:00 de segunda à sexta
+            </li>
+        </ul>
+        <!-- Texto de direitos autorais -->
+        <p>&copy; <?= date('Y'); ?> Imobiliária IS imóveis. Todos os direitos reservados. CRECI 5201-J</p>
+    </div>
 </footer>
-</body>
-
-</html>
